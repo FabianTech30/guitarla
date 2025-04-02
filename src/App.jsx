c99de348c08bb020b863cf510e042c1d71e4b5ae
FabianTech30 <fabybaldenegro@gmail.com>
2025-04-02 09:58:51 -0700

    funcionslist

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { db } from './data/db'
 function App() {
   
   const [data, setData] = useState(db)
-
+  const [cart, setCart] = useState([])
   
 
   return (
@@ -21,8 +21,9 @@ function App() {
         <div className="row mt-5">
           {data.map((guitar) => (
             <Guitar 
-            key={guitar.id}
+              key={guitar.id}
               guitar={guitar}
+              setCart={setCart}
             />
           )
           )}
